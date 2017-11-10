ea268fddf4a019dffe99b8e340848cc7d9eb31e7
lukakosanin <luka.kosanin@gmail.com>
2017-11-10 11:25:03 -0800

    Setting the window title

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QString>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+
     ui->setupUi(this);
+    QString WindowTitle("CSV Reader");
+     this->setWindowTitle(WindowTitle);
 }
 
 MainWindow::~MainWindow()
